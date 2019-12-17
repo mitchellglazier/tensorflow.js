70060261ec39b6681bda70693b7f022c93f7f76c
Mitchell Glazier <mitchell.glazier@telenotes.com>
2019-12-16 23:06:58 -0700

    first commit

---
+++ MobileNet/main.js
@@ -0,0 +1,54 @@
+function setup() {
+  let video = document.getElementById("video");
+  let canvas = document.getElementById("canvas");
+  let pre = document.getElementById("predictions");
+  let model = null;
+
+  async function startCamera() {
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    await video.play();
+
+    setInterval(() => takeSnapshot(), 1000);
+  }
+
+  function takeSnapshot() {
+    let context = canvas.getContext("2d"),
+      width = video.videoWidth,
+      height = video.videoHeight;
+
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(video, 0, 0, width, height);
+
+      classifyImage();
+    }
+  }
+
+  async function classifyImage() {
+    let predictions = [];
+    predictions = await model.classify(canvas);
+    displayPredictions(predictions);
+  }
+
+  function displayPredictions(predictions) {
+    console.log(predictions);
+    let val = "";
+
+    for (prediction of predictions) {
+      let perc = (prediction.probability * 100).toFixed(2);
+      val += `${perc}% | ${prediction.className}\n`;
+      console.log(val);
+    }
+    pre.innerHTML = val
+  }
+
+  async function main() {
+    model = await mobilenet.load();
+    await startCamera();
+  }
+ main()
+}
+  
